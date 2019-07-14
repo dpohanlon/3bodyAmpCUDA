f2ec968fc96374f01ca86a2edaeddef2ea2d533e
Daniel O'Hanlon <dpohanlon@gmail.com>
2019-07-14 14:01:40 +0200

    Add CPU code for comparison

--- spinTermCalc.cu
+++ spinTermCalc.cu
@@ -50,8 +50,8 @@ public:
 	std::vector<float> * spinTerms;
 
 	// Deal with these guys later...
-	static const int spin = 5;
-	static const int spinType = 0;
+	static const int spin = 4;
+	static const int spinType = 1;
 
 	SpinTermParams(int s)
 	{
@@ -185,49 +185,49 @@ public:
 // Spin functions
 
 template<typename Spin>
-__device__
+__host__ __device__
 float legFunc(float cosHel)
 {
     return 1.0;
 }
 
 template<>
-__device__
+__host__ __device__
 float legFunc<Int2Type<SPIN0>>(float cosHel)
 {
     return 1.0;
 }
 
 template<>
-__device__
+__host__ __device__
 float legFunc<Int2Type<SPIN1>>(float cosHel)
 {
     return -2.0 * cosHel;
 }
 
 template<>
-__device__
+__host__ __device__
 float legFunc<Int2Type<SPIN2>>(float cosHel)
 {
     return 4.0*(3.0*cosHel*cosHel - 1.0)/3.0;
 }
 
 template<>
-__device__
+__host__ __device__
 float legFunc<Int2Type<SPIN3>>(float cosHel)
 {
     return -8.0*(5.0*cosHel*cosHel*cosHel - 3.0*cosHel)/5.0;
 }
 
 template<>
-__device__
+__host__ __device__
 float legFunc<Int2Type<SPIN4>>(float cosHel)
 {
     return 16.0*(35.0*cosHel*cosHel*cosHel*cosHel - 30.0*cosHel*cosHel + 3.0)/35.0;
 }
 
 template<>
-__device__
+__host__ __device__
 float legFunc<Int2Type<SPIN5>>(float cosHel)
 {
     return -32.0*(63.0*cosHel*cosHel*cosHel*cosHel*cosHel - 70.0*cosHel*cosHel*cosHel + 15.0*cosHel)/63.0;
@@ -236,42 +236,42 @@ float legFunc<Int2Type<SPIN5>>(float cosHel)
 // Cov factors
 
 template<typename Spin>
-__device__
+__host__ __device__
 float covFactor(float erm)
 {
     return 1.0;
 }
 
 template<>
-__device__
+__host__ __device__
 float covFactor<Int2Type<SPIN0>>(float erm)
 {
     return 1.0;
 }
 
 template<>
-__device__
+__host__ __device__
 float covFactor<Int2Type<SPIN1>>(float erm)
 {
     return erm;
 }
 
 template<>
-__device__
+__host__ __device__
 float covFactor<Int2Type<SPIN2>>(float erm)
 {
     return erm*erm + 0.5;
 }
 
 template<>
-__device__
+__host__ __device__
 float covFactor<Int2Type<SPIN3>>(float erm)
 {
     return erm*(erm*erm + 1.5);
 }
 
 template<>
-__device__
+__host__ __device__
 float covFactor<Int2Type<SPIN4>>(float erm)
 {
     return (8.*erm*erm*erm*erm + 24.*erm*erm + 3.)/35.;
@@ -371,6 +371,8 @@ void calcSpinTerm(const SpinTermParams & inParams)
 
 	bool covariant = inParams.spinType == COVARIANT;
 
+    std::cout << covariant << std::endl;
+
 	KernelParams * kParams = new KernelParams();
 
 	kParams->cosHel = inParams.cosHel;
@@ -399,77 +401,33 @@ void calcSpinTerm(const SpinTermParams & inParams)
 	delete kParams;
 }
 
-// void calcSpinTerm(SpinTermParams inParams)
-// {
-//
-// 	int n = inParams.cosHel->size;
-//
-// 	bool covariant = inParams.spinType == COVARIANT;
-//
-//     std::cout << n << " " << covariant << std::endl;
-//
-// 	KernelParams kParams;
-//
-// 	cudaMallocManaged(&kParams.spinTerms, n * sizeof(float));
-// 	cudaMallocManaged(&kParams.leg, n * sizeof(float));
-// 	cudaMallocManaged(&kParams.cosHel, n * sizeof(float));
-// 	cudaMallocManaged(&kParams.p, n * sizeof(float));
-// 	cudaMallocManaged(&kParams.q, n * sizeof(float));
-// 	if (covariant) cudaMallocManaged(&kParams.erm, n * sizeof(float));
-//
-// 	// Init on device (we can do that as memory is 'unified')
-//
-// 	kParams.cosHel = inParams.cosHel->data();
-// 	kParams.p = inParams.p->data();
-// 	kParams.q = inParams.q->data();
-// 	if (covariant) kParams.erm = inParams.erm->data();
-//
-// 	int device = -1;
-//
-// 	cudaGetDevice(&device);
-//
-// 	cudaMemPrefetchAsync(kParams.spinTerms, n * sizeof(float), device, NULL);
-// 	cudaMemPrefetchAsync(kParams.leg, n * sizeof(float), device, NULL);
-// 	cudaMemPrefetchAsync(kParams.cosHel, n * sizeof(float), device, NULL);
-// 	cudaMemPrefetchAsync(kParams.p, n * sizeof(float), device, NULL);
-// 	cudaMemPrefetchAsync(kParams.q, n * sizeof(float), device, NULL);
-// 	if (covariant) cudaMemPrefetchAsync(kParams.erm, n * sizeof(float), device, NULL);
-//
-// 	int blockSize = 128;
-// 	int numBlocks = (n + blockSize - 1) / blockSize;
-//
-// 	// spinTermKern<Int2Type<SPIN1>, Int2Type<ZEMACH>><<<numBlocks, blockSize>>>(n, cosHel, p, q, out, leg);
-// 	// spinTermKern<Int2Type<spin>, Int2Type<spinType>><<<numBlocks, blockSize>>>(n, cosHel, p, q, out, leg);
-//
-// 	if (!covariant) {
-// 		spinTermZemachKern<Int2Type<inParams.spin>><<<numBlocks, blockSize>>>(n, kParams);
-// 	} else {
-// 		spinTermCovKern<Int2Type<inParams.spin>><<<numBlocks, blockSize>>>(n, kParams);
-// 	}
-//
-// 	cudaError_t cudaStatus = cudaDeviceSynchronize();
-//
-// 	if (cudaStatus != cudaSuccess) {
-// 	    std::cout << "sync failed" << std::endl;
-// 	}
-//
-// 	inParams.leg->insert(inParams.leg->end(), &kParams.leg[0], &kParams.leg[n]);
-// 	inParams.spinTerms->insert(inParams.spinTerms->end(), &kParams.spinTerms[0], &kParams.spinTerms[n]);
-// }
+void calcSpinTermCPU(const SpinTermParams & inParams)
+{
+
+    for (int i = 0; i < inParams.cosHel->size(); i++) {
+        inParams.leg->at(i) = legFunc<Int2Type<inParams.spin>>(inParams.cosHel->at(i));
+		float pProd = inParams.p->at(i) * inParams.q->at(i);
+		inParams.spinTerms->at(i) = inParams.leg->at(i) * pow(pProd, inParams.spin) * covFactor<Int2Type<inParams.spin>>(inParams.erm->at(i));
+    }
+}
 
 int main(int argc, char const *argv[]) {
 
-    SpinTermParams pars(int(1E4));
+    SpinTermParams pars(int(1E8));
+
+	// Things to consider:
+	// floats -> keep an eye on the precision
+	// Might want to keep some things in GPU memory (e.g., spin terms) for further calculations
 
 	std::fill(pars.cosHel->begin(), pars.cosHel->end(), 0.2);
-	std::fill(pars.q->begin(), pars.q->end(), 10.);
-	std::fill(pars.p->begin(), pars.p->end(), 10.);
-	std::fill(pars.erm->begin(), pars.erm->end(), 1000.);
+	std::fill(pars.q->begin(), pars.q->end(), 0.05);
+	std::fill(pars.p->begin(), pars.p->end(), 0.003);
+	std::fill(pars.erm->begin(), pars.erm->end(), 3.3);
 	std::fill(pars.leg->begin(), pars.leg->end(), 1.0);
 	std::fill(pars.spinTerms->begin(), pars.spinTerms->end(), 1.0);
 
-	// calcLegendrePolyManaged(pars);
-	calcSpinTerm(pars);
+	// calcSpinTerm(pars);
+	calcSpinTermCPU(pars);
 
 	std::cout << (pars.leg)->at(5) << std::endl;
 	std::cout << (pars.spinTerms)->at(5) << std::endl;
