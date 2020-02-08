4c7c71adaa5b25e9e35b7adc101ae5bdaaeb7d74
Daniel O'Hanlon <dpohanlon@gmail.com>
2020-02-08 23:55:46 +0000

    Fix some memory errors.

--- splineCalc.cu
+++ splineCalc.cu
@@ -114,7 +114,7 @@ public:
 };
 
 __global__
-void evalSplineKern(const int n, const FloatArr & xs, const SplineParams & params, FloatArr & splineVals)
+void evalSplineKern(const int n, const FloatArr * xs, const SplineParams * params, FloatArr * splineVals)
 {
     // All threads handle blockDim.x * gridDim.x
     // consecutive elements (interleaved partitioning)
@@ -127,69 +127,88 @@ void evalSplineKern(const int n, const FloatArr & xs, const SplineParams & param
         int cell(0);
 
         // Try and avoid this on GPU if possible -> precompute and save?
-        while( xs[i] > params.knotsX[cell+1] ) {
+        while( (*xs)[i] > params->knotsX[cell+1] ) {
             ++cell;
         }
 
         // Might be a slow memory access....
 
-        float xLow  = params.knotsX[cell];
-        float xHigh = params.knotsX[cell+1];
-        float yLow  = params.knotsY[cell];
-        float yHigh = params.knotsY[cell+1];
+        float xLow  = params->knotsX[cell];
+        float xHigh = params->knotsX[cell+1];
+        float yLow  = params->knotsY[cell];
+        float yHigh = params->knotsY[cell+1];
 
-        float t = (xs[i] - xLow) / (xHigh - xLow);
-        float a = params.dydxs[cell] * (xHigh - xLow) - (yHigh - yLow);
-        float b = -1. * params.dydxs[cell+1] * (xHigh - xLow) + (yHigh - yLow);
+        float t = ((*xs)[i] - xLow) / (xHigh - xLow);
+        float a = params->dydxs[cell] * (xHigh - xLow) - (yHigh - yLow);
+        float b = -1. * params->dydxs[cell+1] * (xHigh - xLow) + (yHigh - yLow);
 
-        splineVals[i] = (1 - t) * yLow + t * yHigh + t * (1 - t) * ( a * (1 - t) + b * t );
+        (*splineVals)[i] = (1 - t) * yLow + t * yHigh + t * (1 - t) * ( a * (1 - t) + b * t );
 
     }
 
 }
 
-void calcSplineGPU(std::vector<float> & knotsX,
-                   std::vector<float> & knotsY,
-                   std::vector<float> & dydxs,
-                   std::vector<float> & masses)
+void calcSplineGPU(std::vector<float> * knotsX,
+                   std::vector<float> * knotsY,
+                   std::vector<float> * dydxs,
+                   std::vector<float> * masses)
 {
-    int n = masses.size();
-    int nKnots = knotsX.size();
+    int n = masses->size();
+    int nKnots = knotsX->size();
 
     SplineParams * splineParams = new SplineParams();
 
-    splineParams->knotsX = &knotsX;
-    splineParams->knotsY = &knotsY;
-    splineParams->dydxs = &dydxs;
+    splineParams->knotsX = knotsX;
+    splineParams->knotsY = knotsY;
+    splineParams->dydxs = dydxs;
 
-    FloatArr xs = &masses;
+    FloatArr * xs = new FloatArr(masses);
 
-    std::vector<float> splineValsV;
-    std::fill(splineValsV.begin(), splineValsV.end(), 0.0);
+    std::vector<float> * splineValsV = new std::vector<float>;
+    splineValsV->resize(n);
 
-    FloatArr splineVals = &splineValsV;
+    std::fill(splineValsV->begin(), splineValsV->end(), 0.0);
+
+    FloatArr * splineVals = new FloatArr(splineValsV);
 
     splineParams->prefetch();
-    xs.prefetch();
-    splineVals.prefetch();
+    xs->prefetch();
+    splineVals->prefetch();
 
     int blockSize = 128;
     int numBlocks = (n + blockSize - 1) / blockSize;
 
     for (int i = 0; i < 100; i++){
-        evalSplineKern<<<numBlocks, blockSize>>>(n, xs, *splineParams, splineVals);
+        evalSplineKern<<<numBlocks, blockSize>>>(n, xs, splineParams, splineVals);
     }
 
     splineParams->sync();
-    xs.sync();
-    splineVals.sync();
+    xs->sync();
+    splineVals->sync();
+
+    std::cout << (*splineVals)[0] << std::endl;
 
     delete splineParams;
 }
 
 int main(int argc, char const *argv[]) {
 
-    // Do stuff.
+    std::vector<float> * knotsX = new std::vector<float>;
+    std::vector<float> * knotsY = new std::vector<float>;
+    std::vector<float> * dydxs = new std::vector<float>;
+    std::vector<float> * masses = new std::vector<float>;
+
+    knotsX->resize(100);
+    knotsY->resize(100);
+    dydxs->resize(100);
+    masses->resize(1000);
+
+    // std::fill(knotsX->begin(), knotsX->end(), 0.05);
+    // std::fill(knotsY->begin(), knotsY->end(), 0.05);
+    // std::fill(dydxs->begin(), dydxs->end(), 0.05);
+    // std::fill(masses->begin(), masses->end(), 0.1);
+
+    calcSplineGPU(knotsX, knotsY, dydxs, masses);
 
     return 0;
 }
